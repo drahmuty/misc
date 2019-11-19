fd11538186b1ea025f5981b4893ae198592dfb7f
drahmuty <35937398+drahmuty@users.noreply.github.com>
2019-11-18 20:27:41 -0600

    Create findmatch.c

---
+++ findmatch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int findmatch(char *, char *);
+
+int main(void)
+{
+    // put something here
+}
+
+int findmatch(char *p, char*t)
+{
+    char *pp, *tt;
+    *pp == *p;
+    
+}
